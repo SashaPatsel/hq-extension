baf75540c5bc01333e3ee577d1d05d7bc20dd41d
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-07-29 13:43:47 -0700

    add cors anywhere

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,10 @@
+// CORS ANYWHERE
+jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+
 // Initialize Firebase
 
 
